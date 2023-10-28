106d5b7d44fde77914ddfe68272af686018bf755
Alex Mercer <kalagaev.alex.a@gmail.com>
2023-10-28 20:52:37 +0300

    28.10.2023 / folder style

--- src/mainpage/Mainpage.jsx
+++ src/mainpage/Mainpage.jsx
@@ -1,10 +1,163 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import folder from './media/Folder.svg'
 
 function Mainpage() {
+
+
+
   return (
-    <div>Mainpage f
-      
+    <div className='mainpage'>
+
+      <header className='mainpage__header'>
+        <span>University Projects Showcase</span>
+      </header>
+
+      <span className='line-bottom'></span>
+
+      <main className='mainpage__main'>
+
+        <div className='folder-cell' >
+          
+          <img className='folder-cell__svg'
+          draggable='false'
+          src={folder}
+          alt='Folder' />
+    
+          <div className='folder-cell__description'>
+    
+            <h5>Project name</h5>
+    
+            <div className='description'>
+              <h5>Description:</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eligendi vel, nobis illum nesciunt omnis doloribus. Eveniet quisquam, quas eum deleniti blanditiis vel et hic itaque consectetur odio quasi molestias!</p>
+            </div>
+    
+            <div className='date'>
+              <h5>Date:</h5>
+              <p>12.12.2000 - 12.15.2000</p>
+            </div>
+    
+          </div>
+        </div>
+        <div className='folder-cell' >
+          
+          <img className='folder-cell__svg'
+          draggable='false'
+          src={folder}
+          alt='Folder' />
+    
+          <div className='folder-cell__description'>
+    
+            <h5>Project name</h5>
+    
+            <div className='description'>
+              <h5>Description:</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eligendi vel, nobis illum nesciunt omnis doloribus. Eveniet quisquam, quas eum deleniti blanditiis vel et hic itaque consectetur odio quasi molestias!</p>
+            </div>
+    
+            <div className='date'>
+              <h5>Date:</h5>
+              <p>12.12.2000 - 12.15.2000</p>
+            </div>
+    
+          </div>
+        </div>
+        <div className='folder-cell' >
+          
+          <img className='folder-cell__svg'
+          draggable='false'
+          src={folder}
+          alt='Folder' />
+    
+          <div className='folder-cell__description'>
+    
+            <h5>Project name</h5>
+    
+            <div className='description'>
+              <h5>Description:</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eligendi vel, nobis illum nesciunt omnis doloribus. Eveniet quisquam, quas eum deleniti blanditiis vel et hic itaque consectetur odio quasi molestias!</p>
+            </div>
+    
+            <div className='date'>
+              <h5>Date:</h5>
+              <p>12.12.2000 - 12.15.2000</p>
+            </div>
+    
+          </div>
+        </div>
+        <div className='folder-cell' >
+          
+          <img className='folder-cell__svg'
+          draggable='false'
+          src={folder}
+          alt='Folder' />
+    
+          <div className='folder-cell__description'>
+    
+            <h5>Project name</h5>
+    
+            <div className='description'>
+              <h5>Description:</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eligendi vel, nobis illum nesciunt omnis doloribus. Eveniet quisquam, quas eum deleniti blanditiis vel et hic itaque consectetur odio quasi molestias!</p>
+            </div>
+    
+            <div className='date'>
+              <h5>Date:</h5>
+              <p>12.12.2000 - 12.15.2000</p>
+            </div>
+    
+          </div>
+        </div>
+        <div className='folder-cell' >
+          
+          <img className='folder-cell__svg'
+          draggable='false'
+          src={folder}
+          alt='Folder' />
+    
+          <div className='folder-cell__description'>
+    
+            <h5>Project name</h5>
+    
+            <div className='description'>
+              <h5>Description:</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eligendi vel, nobis illum nesciunt omnis doloribus. Eveniet quisquam, quas eum deleniti blanditiis vel et hic itaque consectetur odio quasi molestias!</p>
+            </div>
+    
+            <div className='date'>
+              <h5>Date:</h5>
+              <p>12.12.2000 - 12.15.2000</p>
+            </div>
+    
+          </div>
+        </div>
+        <div className='folder-cell' >
+          
+          <img className='folder-cell__svg'
+          draggable='false'
+          src={folder}
+          alt='Folder' />
+    
+          <div className='folder-cell__description'>
+    
+            <h5>Project name</h5>
+    
+            <div className='description'>
+              <h5>Description:</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eligendi vel, nobis illum nesciunt omnis doloribus. Eveniet quisquam, quas eum deleniti blanditiis vel et hic itaque consectetur odio quasi molestias!</p>
+            </div>
+    
+            <div className='date'>
+              <h5>Date:</h5>
+              <p>12.12.2000 - 12.15.2000</p>
+            </div>
+    
+          </div>
+        </div>
+
+      </main>
+
     </div>
   )
 }
